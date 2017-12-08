79c660cda43859ab40f88307b8976613d69ba99d
peteman <peteman@localhost>
2017-12-08 11:17:43 -0800

    Overall Math improvement

--- src/org/usfirst/frc/team2659/robot/util/BoilerGear.java
+++ src/org/usfirst/frc/team2659/robot/util/BoilerGear.java
@@ -8,10 +8,16 @@ public class BoilerGear implements PathContainer {
 	 @Override
 	    public Path buildPath() {
 	        ArrayList<Waypoint> sWaypoints = new ArrayList<Waypoint>();
-	        sWaypoints.add(new Waypoint(100,90,0,0));
-	        sWaypoints.add(new Waypoint(85,70,10,50));
-	        sWaypoints.add(new Waypoint(150,40,25,50));
-	        sWaypoints.add(new Waypoint(90,25,0,50));
+	        //sWaypoints.add(new Waypoint(100,90,0,0));
+	        //sWaypoints.add(new Waypoint(85,70,10,50));
+	        //sWaypoints.add(new Waypoint(150,40,30,50));
+	        //sWaypoints.add(new Waypoint(90,25,0,50));
+	        sWaypoints.add(new Waypoint(130,130,0,0));
+	        sWaypoints.add(new Waypoint(110,90,20,50));
+	        sWaypoints.add(new Waypoint(170,30,40,50));
+	        sWaypoints.add(new Waypoint(100,20,25,50));
+	        sWaypoints.add(new Waypoint(70,90,30,50));
+	        sWaypoints.add(new Waypoint(16,90,0,50));
 
 	        return PathBuilder.buildPathFromWaypoints(sWaypoints);
 	        //return PathAdapter.getBlueGearPath();
@@ -19,7 +25,7 @@ public class BoilerGear implements PathContainer {
 	    
 	    @Override
 	    public RigidTransform2d getStartPose() {
-	    		return new RigidTransform2d(new Translation2d(100, 100), Rotation2d.fromDegrees(0.0)); 
+	    		return new RigidTransform2d(new Translation2d(130, 130), Rotation2d.fromDegrees(0.0)); 
 	    }
 
 	    @Override
